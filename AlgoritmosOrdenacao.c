d453ca34625eaf507e1a8ff10bc55022e4824dc8
1Czerwony <ra120114@uem.br>
2023-10-13 12:35:54 -0300

    finalizado

--- AlgoritmosOrdenacao.c
+++ AlgoritmosOrdenacao.c
@@ -3,9 +3,17 @@
 #include <time.h>
 #include <assert.h>
 
+#define TAM 100000
+
+static int vet1[TAM]; // vetor crescente
+static int vet2[TAM]; // vetor decrescente
+static int vet3[TAM]; // vetor aleatório
+
 void mostra_vetor(int vet[], int tam);
 
 void preencher_vetores(int vet1[], int vet2[], int vet3[], int tam);
+
+void testar_algoritmos(FILE *saida, int i);
 
 void copiar_vet(int vet1[], int vet2[], int tam);
 
@@ -13,11 +21,15 @@ void trocar(int *i, int *j);
 
 void flip(int vet[], int iMax);
 
+void maior_menor(int vet[], int tam, int *maior, int *menor);
+
+void juntar(int vet[], int inicio, int meio, int fim);
+
 int verifica_vetor(int vet[], int tam);
 
 int achar_maior(int vet[], int tam, int *indice_maior);
 
-int *gravity_sort(int vet[], int tam);
+int min(int valor1, int valor2);
 
 int *counting_sort(int vet[], int tam);
 
@@ -29,103 +41,102 @@ int *cocktail_sort(int vet[], int tam);
 
 int *pancake_sort(int vet[], int tam);
 
+int *bingo_sort(int vet[], int tam);
+
+int *gnome_sort(int vet[], int tam);
+
+int *tim_sort(int vet[], int tam);
+
+// enumerador de algoritmos
 enum algoritmos {
-    GRAVITY,
     COUNTING,
-    COMB,
     RADIX,
-    COCKTAIL,
-    PANCAKE
+    COMB,
+    TIM,
+    BINGO,
+    PANCAKE,
+    GNOME,
+    COCKTAIL
 };
 
+// vetor de algoritmos
 typedef int* (*FunctionCallback)(int*, int);
 FunctionCallback algoritmos[] = {
-    &gravity_sort, 
     &counting_sort, 
-    &comb_sort, 
     &radix_sort, 
+    &comb_sort, 
+    &tim_sort,
+    &bingo_sort,
+    &pancake_sort,
+    &gnome_sort,
     &cocktail_sort,
-    &pancake_sort
 };
 
 int main() {
-    
-    int tam = 100000; // tamanho dos vetores
-    printf("Tamanho dos vetores: %d\n", tam);
 
-    int vet1[tam]; // vetor crescente
-    int vet2[tam]; // vetor decrescente
-    int vet3[tam]; // vetor aleatório
+    FILE *saida = fopen("saida.txt", "w");
+    
+    printf("Tamanho dos vetores: %d\n", TAM);
+    fprintf(saida, "Tamanho dos vetores: %d\n", TAM);
 
     // gerar vetores
-    preencher_vetores(vet1, vet2, vet3, tam);
-
+    preencher_vetores(vet1, vet2, vet3, TAM);
 
     // testar algoritmos
-    for (int i = 1; i < 6; i++){
+    for (int i = 0; i < 8; i++){
         switch (i) {
-        case GRAVITY:
-            printf("GravitySort: \n");
-            break;
-        
         case COUNTING:
             printf("CountingSort: \n");
+            fprintf(saida, "CountingSort: \n");
             break;
         
         case COMB:
             printf("CombSort: \n");
+            fprintf(saida, "CombSort: \n");
             break;
 
         case RADIX:
             printf("RadixSort: \n");
+            fprintf(saida, "RadixSort: \n");
             break;
 
         case COCKTAIL:
             printf("CocktailSort: \n");
+            fprintf(saida, "CocktailSort: \n");
             break;  
 
         case PANCAKE:
             printf("PancakeSort: \n");
+            fprintf(saida, "PancakeSort: \n");
+            break;
+
+        case BINGO:
+            printf("BingoSort: \n");
+            fprintf(saida, "BingoSort: \n");
+            break;
+
+        case GNOME:
+            printf("GnomeSort: \n");
+            fprintf(saida, "GnomeSort: \n");
+            break;
+
+        case TIM:
+            printf("TimSort: \n");
+            fprintf(saida, "TimSort: \n");
             break;
         }
-        printf("   Calculando..\n");
-
-        clock_t start, end;
-        double cpu_time_used;
         
-        start = clock();
-        int *vet_cresc = algoritmos[i](vet1, tam);
-        end = clock();
-        printf("   Tempo de execução para vetor crescente: %.3f s\n", ((double) (end - start)) / CLOCKS_PER_SEC);
-
-        start = clock();
-        int *vet_decresc = algoritmos[i](vet2, tam);
-        end = clock();
-        printf("   Tempo de execução para vetor decrescente: %.3f s\n", ((double) (end - start)) / CLOCKS_PER_SEC);
-
-        start = clock();
-        int *vet_aleat = algoritmos[i](vet3, tam);
-        end = clock();
-        printf("   Tempo de execução para vetor aleatório: %.3f s\n", ((double) (end - start)) / CLOCKS_PER_SEC);
-
-        assert(verifica_vetor(vet_cresc, tam));
-        assert(verifica_vetor(vet_decresc, tam));
-        assert(verifica_vetor(vet_aleat, tam));
-        printf("   OK\n");
-        printf("\n");
-
-        free(vet_cresc);
-        free(vet_decresc);
-        free(vet_aleat);
+        testar_algoritmos(saida, i);
+        
     }
+    fclose(saida);
 
     return 0;
 }
 
 void mostra_vetor(int vet[], int tam) {
-    for (int i = 0; i < tam; i++) {
+    for (int i = 0; i < tam; i++)
         printf("%d\t", vet[i]);
-    }
     printf("\n");
 }
 
@@ -133,27 +144,57 @@ void preencher_vetores(int vet1[], int vet2[], int vet3[], int tam) {
     srand(time(NULL));
     int x = rand() % tam;
 
-    // printf("\nVetor 1:");
+    // vetor crescente
     for (int i = 0; i < tam; i++){
         vet1[i] = x;
         x++;
-        // printf(" %d", vet1[i]);
-
     }
 
-    // printf("\nVetor 2:");
+    // vetor decrescente
     for (int i = 0; i < tam; i++){
         vet2[i] = x;
         x--;
-        // printf(" %d", vet2[i]);
     }
 
-    // printf("\nVetor 3:");
-    for (int i = 0; i < tam; i++) {
+    // vetor aleatório
+    for (int i = 0; i < tam; i++)
         vet3[i] = rand() % tam;
-        // printf(" %d", vet3[i]);
-    }
-    // printf("\n\n");
+}
+
+void testar_algoritmos(FILE *saida, int i) {
+    printf("   Calculando..\n");
+    clock_t start, end;
+    double cpu_time_used;
+    
+    start = clock();
+    int *vet_cresc = algoritmos[i](vet1, TAM);
+    end = clock();
+    printf("   Tempo de execução para vetor crescente: \t%.3f s\n", ((double) (end - start)) / CLOCKS_PER_SEC);
+    fprintf(saida, "   Tempo de execução para vetor crescente: \t%.3f s\n", ((double) (end - start)) / CLOCKS_PER_SEC);
+
+    start = clock();
+    int *vet_decresc = algoritmos[i](vet2, TAM);
+    end = clock();
+    printf("   Tempo de execução para vetor decrescente: \t%.3f s\n", ((double) (end - start)) / CLOCKS_PER_SEC);
+    fprintf(saida, "   Tempo de execução para vetor decrescente: \t%.3f s\n", ((double) (end - start)) / CLOCKS_PER_SEC);
+
+
+    start = clock();
+    int *vet_aleat = algoritmos[i](vet3, TAM);
+    end = clock();
+    printf("   Tempo de execução para vetor aleatório: \t%.3f s\n", ((double) (end - start)) / CLOCKS_PER_SEC);
+    fprintf(saida, "   Tempo de execução para vetor aleatório: \t%.3f s\n", ((double) (end - start)) / CLOCKS_PER_SEC);
+
+    assert(verifica_vetor(vet_cresc, TAM));
+    assert(verifica_vetor(vet_decresc, TAM));
+    assert(verifica_vetor(vet_aleat, TAM));
+    
+    printf("   OK\n");
+    printf("\n");
+
+    free(vet_cresc);
+    free(vet_decresc);
+    free(vet_aleat);
 }
 
 void copiar_vet(int vet1[], int vet2[], int tam) {
@@ -175,6 +216,58 @@ void flip(int vet[], int iMax) {
         vet[iMax] = temp;
         start++;
         iMax--;
+    }
+}
+
+void maior_menor(int vet[], int tam, int *maior, int *menor) {
+    *maior = vet[0];
+    *menor = vet[0];
+    for (int i = 0; i < tam; i++) {
+        if (vet[i] > *maior) 
+            *maior = vet[i];
+        if (vet[i] < *menor) 
+            *menor = vet[i];
+    }
+}
+
+void juntar(int vet[], int inicio, int meio, int fim) {
+
+    // separar vetor em 2 subvetores
+    int tam1 = meio - inicio + 1;
+    int tam2 = fim - meio;
+    int esq[tam1], dir[tam2];
+    for (int i = 0; i < tam1; i++)
+        esq[i] = vet[inicio + i];
+    for (int i = 0; i < tam2; i++)
+        dir[i] = vet[meio + 1 + i];
+
+    int i = 0, j = 0, k = inicio;
+
+    // juntar subvetores em unico subvetor ordenado
+    while (i < tam1 && j < tam2){
+        if (esq[i] <= dir[j]) {
+            vet[k] = esq[i];
+            i++;
+        }
+        else {
+            vet[k] = dir[j];
+            j++;
+        }
+        k++;
+    }
+
+    // copiar elementos restantes da esquerda
+    while (i < tam1){
+        vet[k] = esq[i];
+        k++;
+        i++;
+    }
+    
+    // copiar elementos restantes da direita
+    while (i < tam1){
+        vet[k] = dir[j];
+        k++;
+        j++;
     }
 }
 
@@ -201,37 +294,13 @@ int achar_maior(int vet[], int tam, int* indice_maior){
     return max;
 }
 
-int *gravity_sort(int vet[], int tam) {
-
-    // achar maior valor
-    int max = achar_maior(vet, tam, NULL);
-    // printf("Achou maior: %d\n");
-
-    // alocar memória
-    int* aux[tam];
-    for (int i = 0; i < tam; i++)
-        aux[i] = (int*)calloc(max, sizeof(int));
-
-    // marcar beads/bolinhas
-    for (int i = 0; i < tam; i++) 
-        for (int j = 0; j < vet[i]; j++)
-            aux[i][j] = 1;
-    
-    // mover bolinhas para baixo
-    int *resultado = calloc(tam, sizeof(int));
-    for (int j = 0; j < max; j++) {
-        int soma = 0;
-        for (int i = 0; i < tam; i++) {
-            soma += aux[i][j];
-            aux[i][j] = 0;
-        }
-        // preencher resultado
-        for (int i = tam - 1; i >= tam - soma; i--)
-            resultado[i] = j+1;
-    }
-    free(aux);
-
-    return resultado;
+int min(int valor1, int valor2) {
+    if (valor1 > valor2)
+        return valor2;
+    else if (valor2 > valor1)
+        return valor1;
+    else
+        return valor1;
 }
 
 int *counting_sort(int vet[], int tam) {
@@ -397,6 +466,105 @@ int *pancake_sort(int vet[], int tam) {
             
             // Agora move o máximo número para o final
             flip(resultado, tamAtual-1);
+        }
+    }
+
+    return resultado;
+}
+
+int *bingo_sort(int vet[], int tam) {
+
+    // declaração de variáveis	
+    int bingo = vet[0];
+    int prox_bingo = vet[0];
+    maior_menor(vet, tam, &prox_bingo, &bingo);
+    int maior_elem = prox_bingo;
+    int pos_prox_elem = 0;
+
+    // alocar memória para o vetor de resultado
+    int *resultado = malloc(tam * sizeof(int));
+    copiar_vet(resultado, vet, tam);
+
+    // ordenar
+    while (bingo < prox_bingo) {
+        // encontrar o próximo elemento
+        int pos_inicio = pos_prox_elem;
+        for (int i = pos_inicio; i < tam; i++) {
+            // mover os menores elemento (bingo) para o local correto
+            if (resultado[i] == bingo) {
+                trocar(&resultado[i], &resultado[pos_prox_elem]);
+                pos_prox_elem++;
+            }
+            // encontrar o proximo bingo
+            else if (resultado[i] < prox_bingo) {
+                prox_bingo = resultado[i];
+            }
+        }
+        bingo = prox_bingo;
+        prox_bingo = maior_elem;
+    }
+    
+    return resultado;
+}
+
+int *gnome_sort(int vet[], int tam) {
+    int indice = 0;
+
+    int *resultado = malloc(tam * sizeof(int));
+    copiar_vet(resultado, vet, tam);
+
+    while (indice < tam) {
+        if (indice == 0)
+            indice++;
+        if (resultado[indice] >= resultado[indice-1]) 
+            indice++;
+        else {
+            trocar(&resultado[indice], &resultado[indice-1]);
+            indice--;
+        }
+    }
+    
+    return resultado;
+}
+
+int *tim_sort(int vet[], int tam) {
+    
+    // definir tamanho dos subvetores a serem ordenados
+    int run = 1000;
+
+    // alocar memória para vetor de resultado
+    int *resultado = malloc(tam * sizeof(int));
+    copiar_vet(resultado, vet, tam);
+
+    // fazer insertion-sort para cada subvetor de tamanho = run
+    for (int i = 0; i < tam; i += run) {
+        int fim = min((i + run - 1), (tam - 1));
+
+        for (int j = i + 1; j <= fim; j++) {
+            int temp = resultado[j];
+            int k = j - 1;
+            while (k >= i && resultado[k] > temp){
+                resultado[k + 1] = resultado[k];
+                k--;
+            }
+            resultado[k + 1] = temp;
+        }
+    }
+    
+
+    // juntar subvetores a partir do tamanho de run
+    for (int tamanho = run; tamanho < tam; tamanho = 2 * tamanho) {
+        // selecionar ponto de partida do subvetor da esquerda
+        for (int inicio = 0; inicio < tam; inicio += 2 * tamanho) {
+            // achar o ponto final do subvetor da esquerda
+            int meio = inicio + tamanho - 1;
+            // achar fim do subvetor da direita
+            int fim = min((inicio + (2 * tamanho) - 1), (tam - 1));
+            // meio + 1 é o inicio do subvetor da direita
+
+            // juntar subvetores vet[inicio...meio] e vet[meio+1...fim]
+            if (meio < fim)
+                juntar(resultado, inicio, meio, fim);
         }
     }
 
